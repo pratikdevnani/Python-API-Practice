649e2a253b0be1707c2e301f0955aa4f2490ec3c
Pratik Devnani <43350493+pratikdevnani@users.noreply.github.com>
2023-02-10 14:29:46 -0500

    Added a post method and a route

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app = FastAPI()
 
@@ -10,3 +11,11 @@ async def root():
 @app.get("/posts")
 def get_posts():
     return {"data" : "This is your posts"}
+
+#post method for storing data from post request
+@app.post("/createposts")
+#automatically extracts all info from body and stores in a dictionary with name "payload"
+def create_posts(payload: dict = Body(...)):
+    #prints the payload on the terminal
+    print(payload)
+    return {"new_post" : f"title : {payload['title']}, content : {payload['content']}"}
