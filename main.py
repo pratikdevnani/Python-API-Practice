9c3786a41aefa61dac0657474cf123424b8d7158
Pratik Devnani <43350493+pratikdevnani@users.noreply.github.com>
2023-02-10 13:50:54 -0500

    added get

--- main.py
+++ main.py
@@ -5,4 +5,8 @@ app = FastAPI()
 #This is called a route or path operation
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Checking Reload"}
+
+@app.get("/posts")
+def get_posts():
+    return {"data" : "This is your posts"}
