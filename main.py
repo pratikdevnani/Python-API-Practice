4eb81103d3eb768d07fb38067db7e701bcd1b1f7
Pratik Devnani <43350493+pratikdevnani@users.noreply.github.com>
2023-02-14 13:57:28 -0500

    added post methods

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
+from random import randrange
 
 app = FastAPI()
 
@@ -19,6 +20,13 @@ class Post(BaseModel):
     #optional field where if the user does not provide, defaults to None
     rating: Optional[int] = None
 
+my_posts = [{'title' : 'title of post 1', "content" : "content of post 1", "id" : 1}, {"title" : "favorite foods", "content" : "pizza", "id" : 2}]
+
+def find_post(id):
+    for p in my_posts:
+        if p["id"] == id:
+            return p
+
 #This is called a route or path operation
 @app.get("/")
 async def root():
@@ -26,24 +34,37 @@ async def root():
 
 @app.get("/posts")
 def get_posts():
-    return {"data" : "This is your posts"}
+    # auto conversion to json
+    return {"data" : my_posts}
 
-#post method for storing data from post request
-@app.post("/createposts")
+'''#post method for storing data from post request
+@app.post("/posts")
 #automatically extracts all info from body and stores in a dictionary with name "payload"
 def create_posts(payload: dict = Body(...)):
     #prints the payload on the terminal
     print(payload)
-    return {"new_post" : f"title : {payload['title']}, content : {payload['content']}"}
+    return {"new_post" : f"title : {payload['title']}, content : {payload['content']}"}'''
 
 #accepting post data of the format {title str, content str} enforced using pydantic
-@app.post("/createPostsCheck")
-# we reference the post class to give the format that we need the post request body to send
+@app.post("/posts")
+#we reference the post class to give the format that we need the post request body to send
 def create_posts(post : Post):
-    print(post)
+    '''print(post)
     print(post.title)
     print(post.published)
     print(post.rating)
     # to convert pydantic model to dictionary
-    print(post.dict())
-    return {"data" : "new post recieved"}
+    print(post.dict())'''
+
+    post_dict = post.dict()
+    post_dict['id'] = randrange(0, 1000000)
+    my_posts.append(post_dict)
+    return {"data" : post_dict}
+
+#id field is a path parameter
+@app.get("/posts/{id}")
+#we need to validate if this is an integer and then convert else throw automatic error
+def get_post(id : int):
+    #we typecast as int as post request sends the id as a string
+    post = find_post(id)
+    return {"post detail" : post}
